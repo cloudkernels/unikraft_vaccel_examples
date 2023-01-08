33894fc0ac5103791852fa6b3805bd004a6b2ff5
Charalampos Mainas <cmainas@nubificus.co.uk>
2023-01-08 13:44:34 +0000

    Add noop example
    Signed-off-by: Charalampos Mainas <cmainas@nubificus.co.uk>

---
+++ noop.c
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <vaccel.h>
+
+int main()
+{
+	int ret;
+	struct vaccel_session sess;
+
+	ret = vaccel_sess_init(&sess, 0);
+	if (ret != VACCEL_OK) {
+		fprintf(stderr, "Could not initialize session\n");
+		return 1;
+	}
+
+	printf("Initialized session with id: %u\n", sess.session_id);
+
+	ret = vaccel_noop(&sess);
+	printf("Initialized session with id: %u\n", sess.session_id);
+	if (ret) {
+		fprintf(stderr, "Could not run op: %d\n", ret);
+		goto close_session;
+	}
+
+close_session:
+	if (vaccel_sess_free(&sess) != VACCEL_OK) {
+		fprintf(stderr, "Could not clear session\n");
+		return 1;
+	}
+
+	return ret;
+}
