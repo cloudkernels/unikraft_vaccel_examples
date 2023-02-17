954955efe93b38f9748a8ecb013bce0ec76b3057
Charalampos Mainas <cmainas@nubificus.co.uk>
2023-02-17 13:42:45 +0000

    Sgemm example: Use the heap istead of the stack
    The sgemm example uses 3 big arrays (512 x 512) and Unikraft an not
    allocate that much memory in the stack. Thereofre, we need to use the
    heap for these arrays, in the same way as sgemm_generic does.
    
    Signed-off-by: Charalampos Mainas <cmainas@nubificus.co.uk>

--- examples/sgemm.c
+++ examples/sgemm.c
@@ -6,9 +6,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define M 64
-#define N 64
-#define K 64
+#define M 512
+#define N 512
+#define K 512
 
 #define ELEM_2D(array, i, j, ld) (*((array) + i*ld + j))
 
@@ -24,12 +24,13 @@ void init(int m, int n, int k, float *A, float *B, float *C)
 
 	for (int i = 0; i < m; ++i)
 		for (int j = 0; j < n; ++j)
-			ELEM_2D(C, i, j, n) = ((float) i * j) / m;
+			ELEM_2D(C, i, j, n) = 0;
 }
 
 int main(int argc, char *argv[])
 {
 	int data_fp = -1;
+	int ret = VACCEL_EINVAL;
 
 	if (argc == 2) {
 		data_fp = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
@@ -39,15 +40,46 @@ int main(int argc, char *argv[])
 
 	float alpha = 32412.0, beta = 2123.0;
 
-	float A[M*K];
-	float B[K*N];
-	float C[M*N];
+	//float A[M*K];
+	//float B[K*N];
+	//float C[M*N];
+
+	float *A, *B, *C;
+	int64_t *m, *n, *k;
+
+	A = (float*) malloc(M * K * sizeof(float));
+	if (!A) {
+		fprintf(stderr, "Could not allocate memory for Input A matrix\n");
+		ret = VACCEL_ENOMEM;
+		goto free_out;
+	}
+	B = (float*) malloc(K * N * sizeof(float));
+	if (!B) {
+		fprintf(stderr, "Could not allocate memory for target matrix\n");
+		ret = VACCEL_ENOMEM;
+		goto free_out_1;
+	}
+
+	C = (float*) malloc(M * N * sizeof(float));
+	if (!C) {
+		fprintf(stderr, "Could not allocate memory for target matrix\n");
+		ret = VACCEL_ENOMEM;
+		goto free_out_2;
+	}
+
+	m = malloc(sizeof(int64_t));
+	n = malloc(sizeof(int64_t));
+	k = malloc(sizeof(int64_t));
+
+	*m = M;
+	*n = N;
+	*k = K;
 
 	init(M, N, K, A, B, C);
 
 	struct vaccel_session session;
 
-	int ret = vaccel_sess_init(&session, 0);
+	ret = vaccel_sess_init(&session, 0);
 	if (ret)
 		return ret;
 
@@ -70,6 +102,11 @@ int main(int argc, char *argv[])
 
 out:
 	vaccel_sess_free(&session);
-
+	free(C);
+free_out_2:
+	free(A);
+free_out_1:
+	free(B);
+free_out:
 	return ret;
 }
